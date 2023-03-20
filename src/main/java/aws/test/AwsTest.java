d488e8a006964dfe3eccacf979bdfa844e7ad0a2
Vlad Radchenko <vladyslav.radchenko@gmail.com>
2023-03-20 16:33:32 +0000

    Initial commit

---
+++ src/main/java/aws/test/AwsTest.java
@@ -0,0 +1,11 @@
+package aws.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsTest {
+    public static void main(String[] args) {
+        SpringApplication.run(AwsTest.class, args);
+    }
+}
